a7067d1f70f460bb3a773f44528e3fbed134c737
Ascendzor <wow.troymare@gmail.com>
2014-09-15 22:05:39 +1200

    init

---
+++ Viper/Map.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Viper
+{
+    public class Map
+    {
+        public Map()
+        {
+
+        }
+    }
+}
